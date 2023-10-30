1ca6fc0461d6ca5094cb3ed774bfa3d20a96bdc1
shivamishwar07 <shivam.kumar77087@gmail.com>
2023-10-31 00:07:42 +0530

    taskCompleted

--- script.js
+++ script.js
@@ -1,97 +1,150 @@
+
+// let totalPrice = 0;
+// let totalProductPrice=0
+// function saveOnLocal(event) {
+//     event.preventDefault();
+//     const productName = event.target.productName.value;
+//     const price = parseFloat(event.target.price.value);
+//     totalProductPrice += price
+//     const obj = {
+//         productName,
+//         price,
+//     };
+//     totalPrice += price;
+//     const data = axios.post("https://crudcrud.com/api/77b2f45a2f6842fcb8e672781490f644/userData", obj)
+//         .then((response) => {
+//             showOnDisplay(response.data)
+//             updateTotalPrice();
+//         })
+//         .catch((err) => console.log(err))
+
+// }
+// function updateTotalPrice() {
+//     const totalPriceElement = document.getElementById('totalPrice');
+//     totalPriceElement.textContent = `Total Price: $${totalPrice.toFixed(2)}`;
+// }
+// window.addEventListener("DOMContentLoaded", () => {
+//     const data = axios.get("https://crudcrud.com/api/77b2f45a2f6842fcb8e672781490f644/userData")
+//         .then((response) => {
+//             for (var i = 0; i < response.data.length; i++) {
+//                 showOnDisplay(response.data[i]);
+//             }
+//             updateTotalPrice()
+//         })
+// })
+
+// function clearFormData() {
+//     document.getElementById("productName").value = "";
+//     document.getElementById("price").value = "";
+// }
+// function showOnDisplay(obj) {
+//     clearFormData()
+//     const users = document.getElementById('users');
+//     const li = document.createElement('li');
+
+//     li.textContent = `${obj.productName} ${obj.price}`;
+
+//     const button = document.createElement('button');
+//     button.textContent = "Delete";
+//     li.appendChild(button);
+//     users.appendChild(li);
+//     users.appendChild(li);
+
+//     button.onclick = () => {
+//         const id = obj._id;
+//         let deletedPrice = obj.price;
+//         console.log(id);
+//         axios.delete(`https://crudcrud.com/api/77b2f45a2f6842fcb8e672781490f644/userData/${id}`)
+//             .then((response) => {
+//                 totalPrice -= deletedPrice
+//                if(totalPrice<0)
+//                totalPrice=0
+//                updateTotalPrice();
+//             })
+
+//             .catch((err) => console.log(err))
+
+//         users.removeChild(li);
+//     }
+
+
+// }
+
+// Check if the totalPrice is already stored in localStorage and retrieve it
+let totalPrice = parseFloat(localStorage.getItem('totalPrice')) || 0;
+let totalProductPrice = 0;
+
 function saveOnLocal(event) {
     event.preventDefault();
-    const name = event.target.name.value;
-    const email = event.target.email.value;
-    const phone = event.target.phone.value;
+    const productName = event.target.productName.value;
+    const price = parseFloat(event.target.price.value);
+    totalProductPrice += price;
 
     const obj = {
-        name,
-        email,
-        phone
+        productName,
+        price,
     };
 
-    const data = axios.post("https://crudcrud.com/api/ee054ef918684164a0f553fd205235f2/userData", obj)
-        .then((response) => {
-            showOnDisplay(response.data)
-            console.log("User Response"+response);
+    totalPrice += price;
 
+    // Save the updated totalPrice in localStorage
+    localStorage.setItem('totalPrice', totalPrice.toString());
+
+    const data = axios.post("https://crudcrud.com/api/77b2f45a2f6842fcb8e672781490f644/userData", obj)
+        .then((response) => {
+            showOnDisplay(response.data);
+            updateTotalPrice();
         })
         .catch((err) => console.log(err))
-
 }
+
+function updateTotalPrice() {
+    const totalPriceElement = document.getElementById('totalPrice');
+    totalPriceElement.textContent = `Total Price: $${totalPrice.toFixed(2)}`;
+}
+
 window.addEventListener("DOMContentLoaded", () => {
-    const data = axios.get("https://crudcrud.com/api/ee054ef918684164a0f553fd205235f2/userData")
+    const data = axios.get("https://crudcrud.com/api/77b2f45a2f6842fcb8e672781490f644/userData")
         .then((response) => {
             for (var i = 0; i < response.data.length; i++) {
                 showOnDisplay(response.data[i]);
-
             }
+            updateTotalPrice();
         })
 })
+
 function clearFormData() {
-    document.getElementById("name").value = "";
-    document.getElementById("email").value = "";
-    document.getElementById("phone").value = "";
+    document.getElementById("productName").value = "";
+    document.getElementById("price").value = "";
 }
+
 function showOnDisplay(obj) {
-    clearFormData()
+    clearFormData();
     const users = document.getElementById('users');
     const li = document.createElement('li');
 
-    li.textContent = `${obj.name} ${obj.email} ${obj.phone}`;
+    li.textContent = `${obj.productName} ${obj.price}`;
 
     const button = document.createElement('button');
-    // button.appendChild(document.createTextNode('delete'));
     button.textContent = "Delete";
     li.appendChild(button);
     users.appendChild(li);
 
-    const editBtn = document.createElement('button');
-    // button.appendChild(document.createTextNode('delete'));
-    editBtn.textContent = "Edit";
-    li.appendChild(editBtn);
-    users.appendChild(li);
-
     button.onclick = () => {
         const id = obj._id;
-        console.log(id);
-        axios.delete(`https://crudcrud.com/api/ee054ef918684164a0f553fd205235f2/userData/${id}`)
+        let deletedPrice = obj.price;
+        axios.delete(`https://crudcrud.com/api/77b2f45a2f6842fcb8e672781490f644/userData/${id}`)
             .then((response) => {
-                // showOnDisplay(response.data)
-                console.log(response);
-
+                totalPrice -= deletedPrice;
+                if (totalPrice < 0) {
+                    totalPrice = 0;
+                }
+                updateTotalPrice();
+                // Save the updated totalPrice in localStorage after deletion
+                localStorage.setItem('totalPrice', totalPrice.toString());
             })
             .catch((err) => console.log(err))
 
         users.removeChild(li);
     }
-
-    editBtn.onclick = () => {
-        const id = obj._id;
-        console.log(id);
-        document.getElementById('name').value = obj.name;
-        document.getElementById('email').value = obj.email;
-        document.getElementById('phone').value = obj.phone;
-        axios.delete(`https://crudcrud.com/api/ee054ef918684164a0f553fd205235f2/userData/${id}`)
-            .then((response) => {
-                // showOnDisplay(response.data)
-                console.log(response);
-
-            })
-            .catch((err) => console.log(err))
-
-
-
-        // axios.put(`https://crudcrud.com/api/ee054ef918684164a0f553fd205235f2/userData/${id}`)
-        // .then((response) => {
-        //     // showOnDisplay(response.data)
-
-        // })
-        // .catch((err) => console.log(err))
-
-        users.removeChild(li);
-
-    }
-
-
 }
